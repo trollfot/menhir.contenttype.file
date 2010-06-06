121fee4d0b7d56703e3b42af3cb088c8f4804f5f
Souheil CHELFOUH <trollfot@gmail.com>
2010-06-06 17:39:40 +0200

    Changed imports for icons

--- src/menhir/contenttype/file/file.py
+++ src/menhir/contenttype/file/file.py
@@ -3,6 +3,7 @@
 import dolmen.content as content
 from dolmen.file import FileField
 from dolmen.blob import BlobProperty
+from dolmen.app.content import icon
 from dolmen.app.security import content as security
 from zope.i18nmessageid import MessageFactory
 
@@ -21,9 +22,9 @@ class IFile(content.IBaseContent):
 class File(content.Content):
     """A file content type storing the data in blobs.
     """
+    icon("file.png")
     content.name(_(u"File"))
     content.schema(IFile)
-    content.icon("file.png")
     content.require(security.CanAddContent)
 
     data = BlobProperty(IFile['data'])
